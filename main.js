23faa7919ec04d2ff6abb9cf898a881ddec372f9
Josefina <marsalahjosefina@gmail.com>
2025-12-16 20:11:20 +0000

    Cargar archivos

---
+++ main.js
@@ -0,0 +1,87 @@
+document.querySelectorAll('.present-hitbox').forEach(hitbox => {
+    const wrapper = hitbox.closest('.present-wrapper');
+    const visual = wrapper.querySelector('.present-visual');
+    const color = hitbox.dataset.color;
+
+    const normalSrc = `assets/${color}-present.png`;
+    const hoverSrc = `assets/${color}-present-hover.png`;
+
+    hitbox.addEventListener('mouseenter', () => {
+        visual.src = hoverSrc;
+    });
+
+    hitbox.addEventListener('mouseleave', () => {
+        visual.src = normalSrc;
+    });
+});
+
+// ---
+
+const BASE_WIDTH = 1440;
+const BASE_HEIGHT = 900;
+
+function scaleScene() {
+    const scaleX = window.innerWidth / BASE_WIDTH;
+    const scaleY = window.innerHeight / BASE_HEIGHT;
+
+    const scale = Math.min(scaleX, scaleY);
+
+    document.querySelector('.scene').style.transform =
+        `scale(${scale - 0.15})`;
+}
+
+window.addEventListener('resize', scaleScene);
+scaleScene();
+
+// ---
+
+document.querySelectorAll('.present-hitbox').forEach(hitbox => {
+  const wrapper = hitbox.closest('.present-wrapper');
+  const visual = wrapper.querySelector('.present-visual');
+  const color = hitbox.dataset.color;
+
+  const normalSrc = `assets/${color}-present.png`;
+  const hoverSrc  = `assets/${color}-present-hover.png`;
+
+  hitbox.addEventListener('mouseenter', () => {
+    visual.src = hoverSrc;
+  });
+
+  hitbox.addEventListener('mouseleave', () => {
+    visual.src = normalSrc;
+  });
+});
+
+const overlay = document.querySelector('.overlay');
+
+const contents = {
+  green: document.querySelector('.green-present-content'),
+  pink:  document.querySelector('.pink-present-content'),
+  blue:  document.querySelector('.blue-present-content'),
+};
+
+function closeAll() {
+  Object.values(contents).forEach(content => {
+    content.classList.remove('present-content-show');
+  });
+  overlay.classList.remove('overlay-show');
+}
+
+document.querySelectorAll('.present-hitbox').forEach(hitbox => {
+  const color = hitbox.dataset.color;
+  const content = contents[color];
+
+  hitbox.addEventListener('click', () => {
+
+    closeAll();
+
+    content.classList.add('present-content-show');
+    overlay.classList.add('overlay-show');
+  });
+});
+
+Object.values(contents).forEach(content => {
+  content.addEventListener('click', closeAll);
+});
+
+overlay.addEventListener('click', closeAll);
